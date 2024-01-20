1a25afc551aa9a0850d5bb1a55338d09cff4d08e
uttammane1 <uttammaneofficial@gmail.com>
2024-01-20 19:03:02 +0530

    add

--- crazyDeal.js
+++
@@ -1,7 +0,0 @@
-let btns = document.querySelectorAll("#crazy-deals-container button")
-btns.forEach(function(el){
-    console.log(el);
-    el.addEventListener("click",function(){
-        alert("You clicked me");
-    })
-})
